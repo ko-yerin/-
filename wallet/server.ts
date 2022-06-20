6034a689d77459da82b00a0d62be741be77fe951
ko-yerin <rhdpfls12@naver.com>
2022-06-20 09:55:34 +0900

    6월20일  1교시  지갑리스트 함수만듬, 지갑에 관련된 매서드 몇가지 만듬

--- wallet/server.ts
+++ wallet/server.ts
@@ -1,8 +1,11 @@
 import express from 'express'
 import nunjucks from 'nunjucks'
 import { Wallet } from './wallet'
+import axios from 'axios'
 
 const app = express()
+
+//axios관련부분
 
 app.use(express.json())
 app.set('view engine', 'html')
@@ -19,6 +22,13 @@ app.post('/newWallet', (req, res) => {
     res.json(new Wallet())
 })
 
+//만들거
+//list
+//view
+//sendTransaction(글쓰기)
+
 app.listen(3005, () => {
     console.log('지갑 서버 시작', 3005)
 })
+
+//npm run dev:ts ./wallet/server.ts
--- wallet/wallet.ts
+++ wallet/wallet.ts
@@ -1,5 +1,9 @@
 import { randomBytes } from 'crypto'
 import elliptic from 'elliptic'
+import fs from 'fs' //file system 의 약자
+import path from 'path'
+
+const dir = path.join(__dirname, '../data') //__dirname이건 머지??
 
 const ec = new elliptic.ec('secp256k1')
 
@@ -10,10 +14,33 @@ export class Wallet {
     public balance: number
 
     constructor() {
+        //여기가 작동한다라는건 객체를 생성했다 ==개인키를 생성했다
         this.privateKey = this.getPrivateKey()
         this.publicKey = this.getPublicKey()
         this.account = this.getAccount()
-        this.balance = 0
+        this.balance = 0 //잔액
+
+        Wallet.createWallet(this) //static을 썻기때문에 Wallet으로 한거다. 아님 this로 하면됨
+    }
+
+    static createWallet(myWallet: Wallet): void {
+        //return 값이 없기때문에  void로
+        const filename = path.join(dir, myWallet.account)
+        const filecontent = myWallet.privateKey
+        //파일명을  account
+        //파일안내용을  privatekey
+        fs.writeFileSync(filename, filecontent)
+    }
+
+    static getWalletList(): string[] {
+        const files: string[] = fs.readdirSync(dir) //dir 디렉토리에 있는 목록들을 가져오는 매서드
+        return files
+    }
+
+    static getWalletPrivateKey(_account: string) {
+        const filepath = path.join(dir, _account)
+        const filecontent = fs.readFileSync(filepath) //dir라는 변수에 account값을 합친 file을 가져오는 매서드
+        return filecontent.toString()
     }
 
     public getPrivateKey(): string {
